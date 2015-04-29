b17a9464013f529d4ee6556ddddd2154cfeef112
sis <sis@raikon>
2015-04-29 16:30:04 +0200

    EsercizioB: modificato Mapper

--- PrimoProgetto/src/esercizioB/InsiemiProdottiCardMapper.java
+++ PrimoProgetto/src/esercizioB/InsiemiProdottiCardMapper.java
@@ -33,35 +33,26 @@ implements Mapper<Object, Text, EnneWritable, IntWritable> {
 			riga.add(word);
 		}
 		
-		//crezione coppie
-		for(int i=0;i<riga.size();i++)
-			for(int j=i+1;j<riga.size();j++){
-				Text[] tmp= new Text[]{new Text(riga.get(i)),new Text(riga.get(j)),new Text(" "),new Text(" ")};
-				pair.set(tmp);
+		for(int i=0;i<riga.size();i++) {
+			for(int j=i+1;j<riga.size();j++) {
+				Text[] tmp1= new Text[]{new Text(riga.get(i)),new Text(riga.get(j)),new Text(" "),new Text(" ")};
+				//crea coppie
+				pair.set(tmp1);
 				output.collect(pair, uno);
-				//System.out.println(pair+" "+uno);
-			}
-		
-		//creazione triple
-		for(int i=0;i<riga.size();i++)
-			for(int j=i+1;j<riga.size();j++)
-				for(int k=j+1;k<riga.size();k++){
-					Text[] tmp= new Text[]{new Text(riga.get(i)),new Text(riga.get(j)),new Text(riga.get(k)),new Text(" ")};
-					triple.set(tmp);
+				for(int w=j+1;w<riga.size();w++) {
+					Text[] tmp2= new Text[]{new Text(riga.get(i)),new Text(riga.get(j)),new Text(riga.get(w)),new Text(" ")};
+					//crea triple
+					triple.set(tmp2);
 					output.collect(triple, uno);
-					//System.out.println(triple+" "+uno);
-				}
-		
-		//creazione quadruple
-		for(int i=0;i<riga.size();i++)
-			for(int j=i+1;j<riga.size();j++)
-				for(int w=j+1;w<riga.size();w++)
 					for(int k=w+1;k<riga.size();k++){
-						Text[] tmp= new Text[]{new Text(riga.get(i)),new Text(riga.get(j)),new Text(riga.get(w)),new Text(riga.get(k))};
-						quadruple.set(tmp);
+						Text[] tmp3= new Text[]{new Text(riga.get(i)),new Text(riga.get(j)),new Text(riga.get(w)),new Text(riga.get(k))};
+						//crea quadruple
+						quadruple.set(tmp3);
 						output.collect(quadruple, uno);
-						//System.out.println(quadruple+" "+uno);
 					}
+				}
+			}
+		}
 		
 	}
 }
